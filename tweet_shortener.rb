b69772b8ce9df1e0dce904f5bae7ac9f7d9a1a08
Sam Bennrubi <sbennrubi@gmail.com>
2018-11-12 01:26:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,8 +12,9 @@ def word_substituter(tweet)
   tweet_string.join(" ")
 end
 
-def bulk_tweet_shortener
-
+def bulk_tweet_shortener(tweets)
+  tweets.each {|tweet| 
+    puts "#{word_substituter(tweet)}"}
 end
 
 def selective_tweet_shortener
