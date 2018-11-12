627e45ff436b815b112ffb17d2b4c36a305d77c7
Sam Bennrubi <sbennrubi@gmail.com>
2018-11-12 01:41:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,10 +26,9 @@ def selective_tweet_shortener(tweet)
 end 
 
 def shortened_tweet_truncator(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
-  elsif word_substituter(tweet) > 140
-    
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[140] = "..."
+  else
     tweet
   end
 end
